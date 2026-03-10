385c6be5ef64e0c0695ee19aed028e713e8f20f6
zuley-tech <zuley.tech@gmail.com>
2026-03-10 22:24:26 +0530

    Update App.tsx

--- landingPage/src/App.tsx
+++ landingPage/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { HomePage, ProductsPage, ProductDetailPage, OrderSuccessPage, TrackOrderPage, PrivacyPolicyPage, RefundPolicyPage, ShippingPolicyPage, TermsConditionsPage } from './pages';
 
+
+
 function App() {
   return (
     <Router>
